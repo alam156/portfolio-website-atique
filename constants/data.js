3d2768971468d6af06e2ca74edd2050c2cbb54f3
ahad <md.ahadulalam@gmail.com>
2024-05-29 18:47:55 +0600

    first commit

---
+++ constants/data.js
@@ -0,0 +1,139 @@
+const userData = {
+  githubUsername: "alam156",
+  name: "Muhammad Ahad Ul Alam",
+  designation: "Assistant Programmer",
+  avatarUrl: "/avatar.png",
+  email: "md.ahadulalam@gmail.com",
+  phone: "+880 1821819036",
+  address: "Chittagong, Bangladesh.",
+  projects: [
+    {
+      title: "Varnish Cache System",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+      text: "A website that i have developed using Java, Spring Boot and react.A website that i have developed using Java, Spring Boot and react."
+
+    },
+    {
+      title: "CI/CD pipeline implementation",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+      text: "A website that i have developed using Java, Spring Boot and react"
+    },
+    {
+      title: "Varnish Cache System",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+      text: "A website that i have developed using Java, Spring Boot and react.A website that i have developed using Java, Spring Boot and react."
+
+    },
+    {
+      title: "CI/CD pipeline implementation",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+      text: "A website that i have developed using Java, Spring Boot and react"
+    }
+    // {
+    //   title: "Portfolio",
+    //   link: "https://manuarora.in",
+    //   imgUrl: "/portfolio.png",
+    // },
+    // {
+    //   title: "CovidRescue",
+    //   link: "https://covidrescue.co.in",
+    //   imgUrl: "/covidrescue.png",
+    // },
+    // {
+    //   title: "Algochurn",
+    //   link: "https://algochurn.com",
+    //   imgUrl: "/algochurn.png",
+    // },
+    // {
+    //   title: "Playground",
+    //   link: "https://play.placeholdertech.in",
+    //   imgUrl: "/playground.png",
+    // },
+    // {
+    //   title: "Feedmeback",
+    //   link: "https://feedmeback.vercel.app",
+    //   imgUrl: "/feedmeback.png",
+    // },
+    // {
+    //   title: "Devmedium",
+    //   link: "https://devmedium.vercel.app",
+    //   imgUrl: "/devmedium.png",
+    // },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    // currentProject: "Tailwind Master Kit",
+    // currentProjectUrl: "https://tailwindmasterkit.com",
+    currentProject: "",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Technical Writer",
+      company: "Bangladesh Computer Council",
+      year: "2022",
+      endyear: "2024",
+      companyLink: "https://www.bcc.gov.bd",
+      desc: "Contributed in developing caclient for macos. Create a UniSigner which will sign hash. I have also created a website for QuickSign System",
+    },
+    {
+      title: "Senior Software Engineer",
+      company: "Anwar Group of Industries",
+      year: "2021",
+      endyear: "2022",
+      companyLink: "https://anwargroup.net",
+      desc: "Developed customized ERP based on the requirements from fifferent entity of the group",
+    },
+    {
+      title: "Assistant Manager",
+      company: "Abul Khair Group",
+      year: "2019",
+      endyear: "2021",
+      companyLink: "https://abulkhairgroup.com",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Graduate Research Assistent",
+      company: "Florida State University",
+      year: "2018-2019",
+      companyLink: "https://fsu.edu",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Software Engineer",
+      company: "Ipvision inc.",
+      year: "2016-2018",
+      companyLink: "https://ipvision.com",
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
+    },
+    {
+      title: "University",
+      company: "Bangladesh University Of Engineering Technology",
+      year: "2016",
+      companyLink: "https://buet.ac.bd",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1mHCVgzV8P_CZPgCkgMnj7RrEhrMUTPXk/view",
+  socialLinks: {
+    instagram: "https://instagram.com/ahadulalam",
+    twitter: "https://twitter.com/ahadulalam",
+    linkedin: "https://linkedin.com/in/ahadulalam",
+    github: "https://github.com/alam156",
+    facebook: "https://facebook.com/ahadulalam",
+    peerlist: "https://peerlist.io/ahadulalam",
+  },
+};
+
+export default userData;
