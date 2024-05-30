12ac2af61ee8f7f795998b36a1b5719202431371
ahad <md.ahadulalam@gmail.com>
2024-05-30 09:29:15 +0600

    project text added

--- components/Projects.js
+++ components/Projects.js
@@ -18,6 +18,7 @@ export default function Projects() {
               title={proj.title}
               link={proj.link}
               imgUrl={proj.imgUrl}
+              text = {proj.text}
               number={`${idx + 1}`}
             />
           ))}
